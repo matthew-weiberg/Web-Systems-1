abf5a48491148c15c2add9c5ff42dacd138681b9
matthew-weiberg <89779247+matthew-weiberg@users.noreply.github.com>
2021-09-11 19:22:52 -0400

    Add files via upload

---
+++ js/form-validation.js
@@ -0,0 +1,53 @@
+// function to validate the fields are completed by the user
+(function () {
+    'use strict'
+
+    var forms = document.querySelectorAll('.needs-validation')
+
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+        })
+    })()
+
+
+// populate dropdown menu with list of states
+var select = document.getElementById("selectState");
+var options = [
+    'Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia',
+    'Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan',
+    'Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands',
+    'Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia',
+    'Washington','West Virginia','Wisconsin','Wyoming'
+];
+
+for(var i = 0; i < options.length; i++) {
+    var opt = options[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+}
+
+
+$(document).ready(function(){
+    $('#birth-date').mask('00/00/0000');
+    $('#phone-number').mask('(000)-000-0000');
+  });
+  
+
+    $(document).ready(function(){
+     
+      $('#datepicker').datepicker({
+        format: "mm/dd/yyyy",
+        endDate: new Date("2021-09-11")
+      });
+    
+    });
